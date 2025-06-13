f882cf13ce0c87daaca2ec57d2ebb3b40d8fcebc
nunits <131896592+nunits@users.noreply.github.com>
2025-06-13 04:40:34 +0000

    json

--- pretty_json.py
+++ pretty_json.py
@@ -1,14 +1,23 @@
 def main():
     data = [
+        # JSON data copied from Postman for the word "baby"
+        {"word": "little", "score": 1001},
+        {"word": "crying", "score": 1000},
+        {"word": "adorable", "score": 999},
+        {"word": "cute", "score": 998},
+        {"word": "sweet", "score": 997},
+        {"word": "sleepy", "score": 996}
+    ]
 
-            # TODO: Add JSON data here
- ]
+    adjectives = []  # Create an empty list
 
-    list = []
     # TODO: Iterate over the data and extract the adjectives for the word and store them in the list variable
+    for item in data:
+        adjectives.append(item["word"])
 
     # TODO: Print the list variable, Example of print: Adjectives for the word "word" are: [list of adjectives]
-    print()
+    print('Adjectives for the word "baby" are:', adjectives)
+
 
 if __name__ == '__main__':
     main()
